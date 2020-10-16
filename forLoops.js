0521fa08970005de1ca04e3aad8563b5659bf218
GWilson444 <gregorywilsonlmt@gmail.com>
2020-10-16 00:41:45 -0700

    updated js file

--- forLoops.js
+++ forLoops.js
@@ -1,12 +1,12 @@
 console.log("PRINT ALL NUMBERS BETWEEN -10 AND 19");
 
-for(i = -10; i < 20; i++) {
+for(let i = -10; i < 20; i++) {
 	console.log(i);
 }
 
 console.log("PRINT ALL EVEN NUMBERS BETWEEN 10 AND 40");
 
-for(i = 10; i <= 40; i++) {
+for(let i = 10; i <= 40; i++) {
 	if(i % 2 === 0) {
 		console.log(i);
 	}
@@ -14,7 +14,7 @@ for(i = 10; i <= 40; i++) {
 
 console.log("PRINT ALL ODD NUMBERS BETWEEN 300 AND 333");
 
-for(i = 300; i <= 333; i++) {
+for(let i = 300; i <= 333; i++) {
 	if(i % 2 !== 0) {
 		console.log(i);
 	}
@@ -22,7 +22,7 @@ for(i = 300; i <= 333; i++) {
 
 console.log("PRINT ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50");
 
-for(i = 5; i <= 50; i++) {
+for(let i = 5; i <= 50; i++) {
 	if(i % 5 === 0 && i % 3 === 0) {
 		console.log(i);
 	}
